333d1be07e1ba75db8f254f9ab42382e43b0284f
takayuki.inoue <takayuki.inoue@i-studio.co.jp>
2021-09-29 17:03:08 +0900

    #1 imageのプリロード追加

--- README.md
+++ README.md
@@ -6,8 +6,39 @@ npm i @itkyk/preload
 ```
 
 ## How to use?
+### Import this Module
 ```
 import Preload from "@itkyk/preload"
+```
 
+### Use Video Preload
+```
+// HTML
+<video class="js-preload-video" src="" data-video-path="/video/to/path/hoge.mp4" />
+
+// JS
 const videoPreload = new Preload.Video(className, callback);
 ```
+
+### Use Image Preload
+```
+// HTML
+!! not Usage !!
+
+// JS
+const fileNameArray = ["/img/to/path/hoge.jpg","/img/to/path/fuga.jpg"];
+const imagePreload = new Preload.Image(fileNameArray, callback);
+```
+
+## Params
+### VideoPreloader
+| argument | type | default | description | 
+|----------|------|---------|-------------|
+| _className | string | js-preload-video | ターゲットとするクラス名を指定 |
+| _func | function | null | 全てロードした時に発火する関数 |
+
+### ImagePreloader
+| argument | type | default | description | 
+|----------|------|---------|-------------|
+| _fileNameArray | Array<string> | null | プリロードしたい画像のパスを指定(配列で複数指定可) |
+| _func | function | null | 全てロードした時に発火する関数 |
--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,15 @@ class Video{
     private counter: number;
     private readonly target: HTMLCollectionOf<Element>;
     private readonly func:()=>void;
-    constructor(_className:string, func:()=>void){
+    constructor(_className:string="js-preload-video", _func:()=>void){
         this.counter = 0;
         this.target = document.getElementsByClassName(_className);
-        this.func = func;
+        this.func = _func;
         this.preload();
     }
 
     preload = () => {
-        const video:HTMLVideoElement = this.target[this.counter] as HTMLVideoElement;
+        const video = this.target[this.counter] as HTMLVideoElement;
         video.src = video.getAttribute("data-video-path") as string;
         this.target[this.counter].addEventListener("canplaythrough", () => {
             if (this.target.length - 1 === this.counter) {
@@ -24,4 +24,33 @@ class Video{
     }
 }
 
-export default {Video}
+class Image {
+    private counter: number;
+    private readonly filePathArray: Array<string>;
+    private readonly func: ()=>{};
+
+    constructor(_fileNameArray:Array<string>, _func:()=>{}=null) {
+        this.counter = 0;
+        this.filePathArray = _fileNameArray;
+        this.func = _func;
+        this.preload();
+    }
+
+    preload = () => {
+        const image = document.createElement("img") as HTMLImageElement;
+        image.src = this.filePathArray[this.counter];
+        image.onload = () => {
+            this.counter++;
+            if (this.filePathArray.length !== this.counter) {
+                this.preload();
+            } else {
+                this.counter = 0;
+                if (this.func) {
+                    this.func();
+                }
+            }
+        }
+    }
+}
+
+export default {Video, Image};
